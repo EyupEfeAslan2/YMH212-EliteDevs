88c3a09c2e98001685df721dfdf497b7828db369
nursenamendillioglu <nursenamendillioglu137@gmail.com>
2026-03-01 13:36:15 +0300

    feat: dom manipulasyonu ile sayfadan metin cekme eklendi

---
+++ extension/content.js
@@ -0,0 +1,9 @@
+// DOM Manipülasyonu: document.body.innerText komutu, o an açık olan sayfanın
+// gövdesindeki (body) tüm "görünen" metinleri kopyalayıp alır.
+const sayfadakiMetin = document.body.innerText;
+
+// Kodu test edebilmek için sayfadan çektiğimiz veriyi Geliştirici Konsoluna yazdırıyoruz
+console.log("EliteDevs Eklentisi Devrede!");
+console.log("Bu sayfadan başarıyla çekilen metin uzunluğu: " + sayfadakiMetin.length + " karakter.");
+console.log("Çekilen metnin ilk 100 karakteri: ", sayfadakiMetin.substring(0, 100) + "...");
+
